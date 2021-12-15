b21f15721e373f23f39d6c8ccb5ff2e72650f5cc
rizqyfahmi <rizqyfahmi@yahoo.com>
2021-12-15 09:28:58 +0700

    Initial Commit

---
+++ SwiftFundamental/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftFundamental
+//
+//  Created by Ahmad Yulia Rizqy Fahmi on 15/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
